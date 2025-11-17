2d457f430505b74b660a4a6919ded91051c5157f
Konstantin <vibecloud@jvnko.boats>
2025-11-17 20:14:23 +0300

    use online status of beatmap instead of mapset

--- osu.Game/Screens/Play/HUD/SkinnableBeatmapSetOnlineStatusPill.cs
+++ osu.Game/Screens/Play/HUD/SkinnableBeatmapSetOnlineStatusPill.cs
@@ -1,24 +1,12 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
-using System.Threading;
-using JetBrains.Annotations;
-using Microsoft.Extensions.Logging;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Logging;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.Drawables;
-using osu.Game.Configuration;
-using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
-using osu.Game.Localisation.SkinComponents;
-using osu.Game.Rulesets;
-using osu.Game.Rulesets.Mods;
 using osu.Game.Skinning;
 
 namespace osu.Game.Screens.Play.HUD
@@ -47,7 +35,7 @@ namespace osu.Game.Screens.Play.HUD
             base.LoadComplete();
             beatmap.BindValueChanged(b =>
             {
-                beatmapSetOnlineStatusPill.Status = beatmap.Value.BeatmapSetInfo.Status;
+                beatmapSetOnlineStatusPill.Status = beatmap.Value.BeatmapInfo.Status;
             }, true);
         }
 
