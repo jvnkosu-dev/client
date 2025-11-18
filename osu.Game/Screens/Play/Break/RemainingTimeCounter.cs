ed889138a0008f02c18367cd5faf62a264fb5a89
Konstantin <vibecloud@jvnko.boats>
2025-11-18 13:23:44 +0300

    break countdown now uses argon-style counter

--- osu.Game/Screens/Play/Break/RemainingTimeCounter.cs
+++ osu.Game/Screens/Play/Break/RemainingTimeCounter.cs
@@ -2,28 +2,33 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.UserInterface;
-using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
+using osu.Game.Screens.Play.HUD;
 
 namespace osu.Game.Screens.Play.Break
 {
     public partial class RemainingTimeCounter : Counter
     {
-        private readonly OsuSpriteText counter;
+        private readonly ArgonCounterTextComponent counter;
 
         public RemainingTimeCounter()
         {
             AutoSizeAxes = Axes.Both;
-            InternalChild = counter = new OsuSpriteText
-            {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Font = OsuFont.TorusAlternate.With(size: 64, weight: FontWeight.SemiBold),
-            };
+            InternalChild = counter = new ArgonCounterTextComponent(Anchor.Centre);
+            counter.Scale *= 1.25f; // this seems to be the only way to make the counter bigger, I hope I'm wrong
+            counter.WireframeOpacity.BindTo(new BindableFloat(0.125f));
         }
-
-        protected override void OnCountChanged(double count) => counter.Text = ((int)Math.Ceiling(count / 1000)).ToString();
+        private string lookup(char c)
+        {
+            return c.ToString();
+        }
+        protected override void OnCountChanged(double count)
+        {
+            string displayText = ((int)Math.Ceiling(count / 1000)).ToString();
+            counter.Text = displayText;
+            counter.WireframeTemplate = new string('#', displayText.Length);
+        }
     }
 }
