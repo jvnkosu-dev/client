616c0d8ecd0e95fd444b171c0588fdbd44f774cb
Konstantin <vibecloud@jvnko.boats>
2025-11-18 13:55:57 +0300

    make key counters show proper readable key names

--- osu.Game/Screens/Play/HUD/KeyCounterBindingTrigger.cs
+++ osu.Game/Screens/Play/HUD/KeyCounterBindingTrigger.cs
@@ -2,6 +2,8 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Input;
 using osu.Framework.Input.Bindings;
 using osu.Framework.Input.Events;
 
@@ -10,12 +12,33 @@ namespace osu.Game.Screens.Play.HUD
     public partial class KeyCounterBindingTrigger<T> : InputTrigger, IKeyBindingHandler<T>
         where T : struct
     {
+        public IKeyBinding Key { get; }
         public T Action { get; }
 
-        public KeyCounterBindingTrigger(IKeyBinding key, T action)
-            : base(key?.KeyCombination.Keys[0].ToString() ?? $"B{(int)(object)action + 1}")
+        [Resolved]
+        private ReadableKeyCombinationProvider keyCombinationProvider { get; set; } = null!;
+
+        [BackgroundDependencyLoader]
+        private void load(ReadableKeyCombinationProvider keys)
         {
+            keyCombinationProvider = keys;
+        }
+
+        private string getName(IKeyBinding key, T action)
+        {
+            return keyCombinationProvider?.GetReadableString(key.KeyCombination) ?? $"B{(int)(object)action + 1}";
+        }
+        public KeyCounterBindingTrigger(IKeyBinding key, T action)
+            : base("")
+        {
+            Key = key;
             Action = action;
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            Name = getName(Key, Action);
         }
 
         public bool OnPressed(KeyBindingPressEvent<T> e)
