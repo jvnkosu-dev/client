118f07878a8334a97beaf7b16e89da092344c124
Dan Balasescu <smoogipoo@smgi.me>
2025-12-06 17:40:17 +0900

    Allow score panel to animate

--- osu.Game/Screens/Ranking/ScorePanel.cs
+++ osu.Game/Screens/Ranking/ScorePanel.cs
@@ -37,7 +37,7 @@ namespace osu.Game.Screens.Ranking
         /// <summary>
         /// Height of the panel when contracted.
         /// </summary>
-        private const float contracted_height = 385;
+        public const float CONTRACTED_HEIGHT = 385;
 
         /// <summary>
         /// Width of the panel when expanded.
@@ -280,7 +280,7 @@ namespace osu.Game.Screens.Ranking
                     break;
 
                 case PanelState.Contracted:
-                    Size = new Vector2(CONTRACTED_WIDTH, contracted_height);
+                    Size = new Vector2(CONTRACTED_WIDTH, CONTRACTED_HEIGHT);
 
                     topLayerBackground.FadeColour(getColour(contracted_top_layer_colour), RESIZE_DURATION, Easing.OutQuint);
                     middleLayerBackground.FadeColour(getColour(contracted_middle_layer_colour), RESIZE_DURATION, Easing.OutQuint);
